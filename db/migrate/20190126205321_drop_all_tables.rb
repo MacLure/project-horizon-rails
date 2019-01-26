89f99ec0b73fdafde831853f6fee8aad916ff5a0
Gabriela <gab.jo.cuello@gmail.com>
2019-01-26 16:18:32 -0500

    Implementing Seed data

---
+++ db/migrate/20190126205321_drop_all_tables.rb
@@ -0,0 +1,8 @@
+class DropAllTables < ActiveRecord::Migration[5.2]
+  def change
+    # drop_table :admins
+    # drop_table :students
+    # drop_table :cohorts
+    # drop_table :tests
+  end
+end
