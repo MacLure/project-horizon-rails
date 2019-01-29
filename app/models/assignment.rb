bd7e51bbf94c814ee9dd692006e6f6cdaa73d162
Gabriela <gab.jo.cuello@gmail.com>
2019-01-29 15:00:43 -0500

    (fixed associations between students/events/assignments/submissions)

--- app/models/assignment.rb
+++ app/models/assignment.rb
@@ -1,4 +1,4 @@
 class Assignment < ApplicationRecord
   belongs_to :cohort
-  has_and_belongs_to_many :students
+  has_many(:students, {through: :cohort})
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -2,7 +2,10 @@ class Student < ApplicationRecord
   has_secure_password
   belongs_to :cohort
   has_one :student_note
-  has_many :submissions 
+  has_many :submissions
+  has_many(:events, {through: :cohort})
+  has_many(:admins, {through: :cohort})
+  has_many(:assignments, {through: :cohort})
 
 
 
