d4e34ed189f2bc1dc22307284c1d65d44e4c2b7d
Gabriela <gab.jo.cuello@gmail.com>
2019-01-30 14:40:05 -0500

    testing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::API
   before_action :cors_set_access_control_headers
   include Knock::Authenticable
-    after_action :authenticate_student_student
+
 
 
   def handle_options_request
@@ -32,9 +32,7 @@ class ApplicationController < ActionController::API
     end
   end
 
-  def authenticate_student_student
-     authenticate_for Student::Student
-   end
+  
   def current_student
   if params[:auth][:email] && !params[:auth][:is_admin]
     puts "..........................#{@current_user.inspect}"
