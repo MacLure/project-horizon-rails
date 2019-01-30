4870f39dca8842035f41853d526b32fe7ba1fdf1
Gabriela <gab.jo.cuello@gmail.com>
2019-01-29 21:03:05 -0500

    Fix server error caused by helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::API
   before_action :cors_set_access_control_headers
   include Knock::Authenticable
-  helper_method :current_user
+  # helper_method :current_user
 
   def handle_options_request
     head(:ok) if request.request_method == "OPTIONS"
@@ -25,9 +25,9 @@ class ApplicationController < ActionController::API
 
     private
 
-    def logged_in?[]
-      current_user
-    end
+    # def logged_in?[]
+    #   current_user
+    # end
 end
 
 
