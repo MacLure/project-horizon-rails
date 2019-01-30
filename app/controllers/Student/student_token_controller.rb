11fa5d755c7cc82eca4a297950fc3804a9ddd932
Gabriela <gab.jo.cuello@gmail.com>
2019-01-30 14:06:05 -0500

    testing

--- app/controllers/Student/student_token_controller.rb
+++ app/controllers/Student/student_token_controller.rb
@@ -1,5 +1,8 @@
 class Student::StudentTokenController < Knock::AuthTokenController
   skip_before_action :verify_authenticity_token
-
+  def current_student
+  return unless params[:auth][:email] && !params[:auth][:is_admin]
+  @current_user ||= Student.find_by_email(params[:auth][:email])
+  end
 
 end
