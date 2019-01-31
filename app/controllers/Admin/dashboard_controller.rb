81842cde4db66477582a8495adc6d41839519fe5
Gabriela <gab.jo.cuello@gmail.com>
2019-01-31 11:09:04 -0500

    testing admin dashboard's index controller

--- app/controllers/Admin/dashboard_controller.rb
+++ app/controllers/Admin/dashboard_controller.rb
@@ -2,32 +2,34 @@ class Admin::DashboardController < ApplicationController
   before_action :authenticate_admin
   def index
 #can now use current admin as a variable
+    @admins = current_admin
     @admins = Admin.all
-    @cohorts = Cohort.all
 
-    @students = Student.all
-    @student_notes = StudentNote.all
-
-    @assignments = Assignment.all
-    @submissions = Submission.all
-    @submission_comments = SubmissionComment.all
-
-    @company_notes = CompanyNote.all
-    @contact_notes = ContactNote.all
-
-    @events = Event.all
+    # @cohorts = Cohort.all
+    #
+    # @students = Student.all
+    # @student_notes = StudentNote.all
+    #
+    # @assignments = Assignment.all
+    # @submissions = Submission.all
+    # @submission_comments = SubmissionComment.all
+    #
+    # @company_notes = CompanyNote.all
+    # @contact_notes = ContactNote.all
+    #
+    # @events = Event.all
 
     render json: {
       admins: @admins,
-      cohorts: @cohorts,
-      students: @students,
-      student_notes: @student_notes,
-      assignments: @assignments,
-      submissions: @submissions,
-      submission_comments: @submission_comments,
-      company_notes: @company_notes,
-      contact_notes: @contact_notes,
-      events: @events,
+      # cohorts: @cohorts,
+      # students: @students,
+      # student_notes: @student_notes,
+      # assignments: @assignments,
+      # submissions: @submissions,
+      # submission_comments: @submission_comments,
+      # company_notes: @company_notes,
+      # contact_notes: @contact_notes,
+      # events: @events,
     }
 
   end
