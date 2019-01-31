aa1e1939a7b29bccc2d97acdbdc7009f0629fbe1
Malcolm <malcolm@Malcolms-MacBook-Pro.local>
2019-01-31 00:20:44 -0500

    student / submission_comments association

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,11 +1,13 @@
 class Student < ApplicationRecord
   has_secure_password
   belongs_to :cohort
-  has_many :student_comments
+  has_many :student_notes
   has_many :submissions
   has_many(:events, {through: :cohort})
   has_many(:admins, {through: :cohort})
   has_many(:assignments, {through: :cohort})
+  has_many(:sibmission_notes, {through: :submission})
+
 
   def self.from_token_payload payload
     self.find payload["sub"]
